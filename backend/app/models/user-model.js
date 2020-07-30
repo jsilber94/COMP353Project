91a8f46d31e76f10468cbd60d67fd28e862668b1
Jesse Silber <je_silbe@live.concordia.ca>
2020-07-30 14:12:07 -0400

    update user endpoints/mysql statements

--- backend/app/models/user-model.js
+++ backend/app/models/user-model.js
@@ -17,7 +17,7 @@ function getAllUser() {
 
 function getUserById(id) {
   return new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM test WHERE id =${id.id}`, (error, rows) => {
+    db.query(`SELECT * FROM user WHERE user_id =${id}`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
@@ -31,7 +31,7 @@ function getUserById(id) {
 
 function addUser(user) {
   return new Promise((resolve, reject) => {
-    db.query(`INSERT INTO test(name,age,state,country)VALUES('${user.name}','${user.age}','${user.state}','${user.country}')`, (error, rows) => {
+    db.query(`INSERT INTO User(fname,lname,category,email,password_hash,balance,date_last_payment,withdrawel_status)VALUES('${user.fname}','${user.lname}','${user.category}','${user.email}','${user.password_hash}','${user.balance}','${user.date_last_payment}','${user.withdrawel_status}')`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
@@ -45,7 +45,7 @@ function addUser(user) {
 
 function updateUser(id, user) {
   return new Promise((resolve, reject) => {
-    db.query(`UPDATE test set name='${user.name}',age='${user.age}',state='${user.state}',country='${user.country}' WHERE id='${id}'`, (error, rows) => {
+    db.query(`UPDATE test set fname='${user.fname}',lname='${user.lname}',category='${user.category}',email='${user.email}',password_hash='${user.password_hash}',balance='${user.balance}',date_last_payment='${user.date_last_payment}',withdrawel_status='${user.withdrawel_status}' WHERE id='${id}'`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
@@ -59,7 +59,7 @@ function updateUser(id, user) {
 
 function deleteUser(id) {
   return new Promise((resolve, reject) => {
-    db.query(`DELETE FROM test WHERE id='${id}'`, (error, rows) => {
+    db.query(`DELETE FROM user WHERE user_id='${id}'`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
