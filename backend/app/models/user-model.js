6631c95dee110b70a8ceb3516b535adeb2b92644
CoricovMA <coricovacMA@gmail.com>
2020-07-31 16:07:33 -0400

    small changes to user-model, was passing int as string

--- backend/app/models/user-model.js
+++ backend/app/models/user-model.js
@@ -31,7 +31,7 @@ function getUserById(id) {
 
 function addUser(user) {
   return new Promise((resolve, reject) => {
-    db.query(`INSERT INTO User(fname,lname,category,email,password_hash,balance,date_last_payment,withdrawel_status)VALUES('${user.fname}','${user.lname}','${user.category}','${user.email}','${user.password_hash}','${user.balance}','${user.date_last_payment}','${user.withdrawel_status}')`, (error, rows) => {
+    db.query(`INSERT INTO User(fname,lname,category,email,password_hash,balance,date_last_payment,withdrawal_status)VALUES('${user.fname}','${user.lname}','${user.category}','${user.email}','${user.password_hash}',${user.balance},${user.date_last_payment},'${user.withdrawal_status}')`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
@@ -45,7 +45,7 @@ function addUser(user) {
 
 function updateUser(id, user) {
   return new Promise((resolve, reject) => {
-    db.query(`UPDATE user set fname='${user.fname}',lname='${user.lname}',category='${user.category}',email='${user.email}',password_hash='${user.password_hash}',balance='${user.balance}',date_last_payment='${user.date_last_payment}',withdrawel_status='${user.withdrawel_status}' WHERE id='${id}'`, (error, rows) => {
+    db.query(`UPDATE user set fname='${user.fname}',lname='${user.lname}',category='${user.category}',email='${user.email}',password_hash='${user.password_hash}',balance='${user.balance}',date_last_payment='${user.date_last_payment}',withdrawal_status='${user.withdrawal_status}' WHERE id='${id}'`, (error, rows) => {
       if (error) {
         dbFunc.connectionRelease();
         reject(error);
