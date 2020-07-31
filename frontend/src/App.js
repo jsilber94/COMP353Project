bc9382688d7c9856c12442d0f06040ac5795cdcf
David Bod <Davidbodromian@outlook.com>
2020-07-31 11:27:43 -0400

    pokemon
    just a template to base some code off of, don't mind me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
+import Pagination from './Pagination';
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  //function will run every time an argument changes in the given array
+  useEffect(() => {
+    setLoading(true)
+    //cancel will get updated everytime axios is called
+    let cancel
+    axios.get(currentPageUrl, {cancelToken: new axios.CancelToken(c => cancel = c)}
+      ).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+  });
+
+  //Gets called everytime useEffect is called (will cancel previous requests when a new request is made)
+  return () => {cancel()}
+  }, [currentPageUrl])
+
+  //will update the currentPageUrl which, in return, forces useEffect to update as well
+  function goToNextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function goToPrevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <PokemonList pokemon = {pokemon} />
+    <Pagination
+    goToNextPage = {nextPageUrl ? goToNextPage : null}
+    goToPrevPage = {prevPageUrl ? goToPrevPage : null}
+    />
+    </>
   );
 }
 
