587e9902bace4d8d426c3e981759e9a35abfcfc0
Jesse Silber <je_silbe@live.concordia.ca>
2020-08-06 19:48:16 -0400

    database local only

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,29 +1,38 @@
 const mysql = require('mysql');
-const tunnel = require('tunnel-ssh')
+// const tunnel = require('tunnel-ssh');
 
-var config = {
-  username:'a_corico',
-  host:'login.encs.concordia.ca',
-  dstPort: 3306,
-  dstHost: "dyc353.encs.concordia.ca",
-  password:'Alex12345678',
-  connectTimeout: 30000
-};
+// const config = {
+//   username: 'a_corico',
+//   host: 'login.encs.concordia.ca',
+//   dstPort: 3306,
+//   dstHost: 'dyc353.encs.concordia.ca',
+//   password: 'Alex12345678',
+//   connectTimeout: 30000,
+// };
 
-var server = tunnel(config, function (error, server) {
-  if(error){
-      console.log("SSH connection error: " + error);
-  }
-});
+// tunnel(config, (error, server) => {
+//   if (error) {
+//     console.log(`SSH connection error: ${error}`);
+//   } else console.log(server);
+// });
+
+// module.exports = mysql.createPool({
+//   connectionLimit: 100,
+//   port: 3306,
+//   host: 'localhost',
+//   user: 'dyc353_1',
+//   password: '1a2s3d4r',
+//   database: 'dyc353_1',
+//   connectTimeout: 60 * 60 * 1000,
+//   acquireTimeout: 60 * 60 * 1000,
+//   timeout: 60 * 60 * 1000,
+// });
 
 module.exports = mysql.createPool({
   connectionLimit: 100,
   port: 3306,
   host: 'localhost',
-  user: 'dyc353_1',
-  password: '1a2s3d4r',
-  database: 'dyc353_1',
-  connectTimeout  : 60 * 60 * 1000,
-  acquireTimeout  : 60 * 60 * 1000,
-  timeout         : 60 * 60 * 1000,
+  user: 'root',
+  password: 'password',
+  database: 'careerportal',
 });
