8036db89841ebff3e1f688bc38d8f822902f9b70
Davido <35125578+DavidoBod@users.noreply.github.com>
2020-08-08 14:57:33 -0400

    job searching user dashboard (#14)
    can filter through jobs on user dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Header from '../components/layout/Header';
 import UserCategory from '../components/UserCategory';
+import Jobs from '../components/Jobs';
 
 function Dashboard() {
   return (
     <div>
       <Header></Header>
       <UserCategory />
+      <Jobs />
     </div>
   );
 }
